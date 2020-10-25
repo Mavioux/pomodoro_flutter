f766b9fd3bf8bf66a855c5b1fe16aeee053cf21e
Mavioux <pakmarios@gmail.com>
2020-10-25 22:39:25 +0200

    Project Initialization

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Pomodoro());
+}
+
+class Pomodoro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
